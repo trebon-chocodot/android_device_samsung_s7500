fc89e4d6b7320dbf6cec78bfbfb41ce66f30a8ab
Giugiu19 <gpirasking@gmail.com>
2014-03-15 17:21:17 +0100

    upgrade source for build omni_4.4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,10 @@
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2013 OmniROM Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,7 +21,7 @@ include device/samsung/msm7x27a-common/BoardConfigCommon.mk
 TARGET_BOOTLOADER_BOARD_NAME := trebon
 
 ## Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod/trebon_defconfig
+TARGET_KERNEL_CONFIG := trebon_defconfig
 
 ## Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/trebon/bluetooth
---
+++ README.md
@@ -0,0 +1,4 @@
+android_device_samsung_trebon
+=============================
+
+Device configuration for Samsung GT-S7500 
