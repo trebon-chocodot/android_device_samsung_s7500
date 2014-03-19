d04474356ca0422ca8b5573816ff3413152b86d4
Giuseppe <gpirasking@gmail.com>
2014-03-19 15:43:07 +0100

    Update omni_trebon.mk

--- omni_trebon.mk
+++ omni_trebon.mk
@@ -12,17 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#Add omni apns
-PRODUCT_COPY_FILES += \
-       vendor/omni/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
-
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from our omni product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-## Inherit device files
+# Inherit device configuration
 $(call inherit-product, device/samsung/trebon/trebon.mk)
 
 ## Setup device configuration
@@ -36,4 +32,3 @@ PRODUCT_DEVICE := trebon
 ## Bootanimation
 TARGET_SCREEN_HEIGHT := 480
 TARGET_SCREEN_WIDTH := 320
-TARGET_BOOTANIMATION_HALF_RES := true
