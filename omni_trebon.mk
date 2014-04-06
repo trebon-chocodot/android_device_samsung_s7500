3ff152b5820098374162e20dbc1e04cda6be40d9
Giugiu19 <gpirasking@gmail.com>
2014-04-07 00:22:51 +0200

    fix source for compile

--- omni_trebon.mk
+++ omni_trebon.mk
@@ -1,10 +1,10 @@
-# Copyright (C) 2013 OmniROM Project
+# Copyright (C) 2013 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,13 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit Omni GSM telephony parts
-$(call inherit-product, vendor/omni/config/gsm.mk)
+#Add omni apns
+PRODUCT_COPY_FILES += \
+       vendor/omni/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from our omni product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit device configuration
+## Inherit device files
 $(call inherit-product, device/samsung/trebon/trebon.mk)
 
 ## Setup device configuration
@@ -32,3 +36,4 @@ PRODUCT_DEVICE := trebon
 ## Bootanimation
 TARGET_SCREEN_HEIGHT := 480
 TARGET_SCREEN_WIDTH := 320
+TARGET_BOOTANIMATION_HALF_RES := true
--- trebon.mk
+++ trebon.mk
@@ -1,10 +1,10 @@
-# Copyright (C) 2013 OmniROM Project
+# Copyright (C) 2013 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,13 +15,9 @@
 DEVICE_PACKAGE_OVERLAYS += device/samsung/trebon/overlay
 
 ## Build recovery?
-BUILD_RECOVERY := true
+#BUILD_RECOVERY := true
 
 include device/samsung/msm7x27a-common/msm7x27a.mk
-
-## Build torch app
-PRODUCT_PACKAGES += \
-    OmniTorch
 
 ifndef BUILD_RECOVERY
 ## Splash screen
@@ -32,7 +28,5 @@ else
 PRODUCT_COPY_FILES += \
     device/samsung/trebon/rootdir/init-cwm.rle:root/GT-S7500.rle
 endif
-
-ROM_BUILDTYPE := NIGHTLY
 
 $(call inherit-product, vendor/samsung/trebon/blobs.mk)
